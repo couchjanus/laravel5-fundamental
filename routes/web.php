28b96d6fa48c6b271124d49e484820f2b6a5348c
couch Janus <couchjanus@gmail.com>
2018-08-10 08:43:54 +0300

    modified

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hey', function()
+{
+    return 'Hello World';
+});
+
+Route::get('/hell', function() {
+    return view('greeting');
+});
+
+Route::get('/jan', function() {
+    return view('hello.greeting', ['name' => 'Janus']);
+});
+
+Route::get('/about', 'AboutController');
+
+Route::get('/test', 'TestController@index');
+
+Route::get('foo', ['uses' => 'TestController@fooIndex', 'as' => 'name']);
+
+Route::get('bar', 'TestController@barIndex');
+
+Route::get('bax', 'TestController@baxIndex');
+
+Route::get('baz', 'TestController@bazIndex');
